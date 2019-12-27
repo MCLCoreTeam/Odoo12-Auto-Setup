436857a038af552f6bc43d4169befce9d092067e
MCLCoreTeam <48421452+MCLCoreTeam@users.noreply.github.com>
2019-12-27 07:31:23 +0300

    Update odoo12-auto.sh

--- odoo12-auto.sh
+++ odoo12-auto.sh
@@ -22,7 +22,6 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install git python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less
 echo "---------------"
-# Install nanomsg:
 
 echo "Create Odoo user"
 echo
